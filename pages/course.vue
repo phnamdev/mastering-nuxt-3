59a0485d1edcc43ad6d1a6a86dd10fab87035d6d
TrippleN <idlogin97@gmail.com>
2022-12-14 22:16:30 +0700

    :+1: File based routing

---
+++ pages/course.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="flex flex-col items-center w-full h-full min-h-screen p-12 bg-gray-100"
+  >
+    <div class="mb-12 prose">
+      <h1>
+        <span class="font-medium">
+          Course:
+          <span class="font-bold">Mastering Nuxt 3</span>
+        </span>
+      </h1>
+    </div>
+
+    <div class="flex flex-row justify-center flex-grow">
+      <div
+        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
+      >
+        <h3>Chapters</h3>
+        <!-- All the lessons for the course listed here -->
+      </div>
+
+      <div class="prose p-12 bg-white rounded-md w-[65ch]">
+        <h2>Lesson</h2>
+        <p>This is a lesson</p>
+      </div>
+    </div>
+  </div>
+</template>
